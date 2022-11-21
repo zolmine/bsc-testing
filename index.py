4e80e87ca475ecfb53eb67a2d78647ae4617beb2
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-11-21 15:00:03 +0100

    Update index.py

--- index.py
+++ index.py
@@ -27,7 +27,7 @@ print(f"GetTransactionsByBlockNumber tooks {(time()*1000) - start} ms, result ==
 gasFunc02 = str(dumps({
   "jsonrpc": "2.0",
   "id": 0,
-  "method": "eth_getTransactionByBlockNumberAndIndex",
+  "method": "eth_getTransactionByBlockNumberAndIndex1",
   "params": [
     "pending",
     True
