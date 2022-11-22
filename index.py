7e0a77d33ad0c483921c9498b1925788c4a6b707
Mohammed Amine Essobhi <68522174+zolmine@users.noreply.github.com>
2022-11-22 12:23:32 +0100

    Update index.py

--- index.py
+++ index.py
@@ -4,26 +4,26 @@ from time import time
 
 link = "ws://127.0.0.1:8546"
 ws = create_connection(link)
-gasFunc02 = str(dumps({
-  "jsonrpc": "2.0",
-  "id": 0,
-  "method": "eth_getBlockByNumber",
-  "params": [
-    "latest",
-    True
-  ]
-}), "utf-8")
 # gasFunc02 = str(dumps({
 #   "jsonrpc": "2.0",
 #   "id": 0,
-#   "method": "eth_getTransactionByHash01Pending",
+#   "method": "eth_getBlockByNumber",
 #   "params": [
-#     "pending"
+#     "latest",
+#     True
 #   ]
 # }), "utf-8")
-start = time() * 1000
-ws.send(gasFunc02)
-print(f"GetTransactionsByBlockNumber tooks {(time()*1000) - start} ms, result ==  {ws.recv()}" )
+# # gasFunc02 = str(dumps({
+# #   "jsonrpc": "2.0",
+# #   "id": 0,
+# #   "method": "eth_getTransactionByHash01Pending",
+# #   "params": [
+# #     "pending"
+# #   ]
+# # }), "utf-8")
+# start = time() * 1000
+# ws.send(gasFunc02)
+# print(f"GetTransactionsByBlockNumber tooks {(time()*1000) - start} ms, result ==  {ws.recv()}" )
 gasFunc02 = str(dumps({
   "jsonrpc": "2.0",
   "id": 0,
